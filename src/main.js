54a9626f80f178c44d6ce299c5a22e9ae82b5ff5
inesbad <inesbadjou213@gmail.com>
2025-12-10 11:15:11 +0100

    version finale

--- src/main.js
+++ src/main.js
@@ -1,60 +1,126 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+let offset = 0
+const personCards = []
 
-// setupCounter(document.querySelector('#counter'))
+// Conteneur principal
+const div = document.getElementById("app")
 
-// Fetch
 
+
+// Barre de recherche
+const searchBar = document.createElement("input")
+searchBar.type = "search"
+searchBar.placeholder = "ex : Guy Drut"
+div.appendChild(searchBar)
+
+const btn = document.createElement("button")
+btn.textContent = "Rechercher"
+div.appendChild(btn)
+
+
+
+// Conteneur pour les cards qui stocke toutes les cards des conseilleirs. 
+//Cela me eprmet de les organiser avec CSS
+const cardsContainer = document.createElement("div")
+cardsContainer.id = "cards-container"
+div.appendChild(cardsContainer)
+
+
+
+// Bouton "Charger plus"
+const loadBtnMore = document.createElement("button")
+loadBtnMore.className = "load-more"
+loadBtnMore.textContent = "Charger plus"
+div.appendChild(loadBtnMore)
+
+
+
+// Fonction fetch API
 async function fetchApi() {
   try {
-    const response = await fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/les-conseillers-de-paris-de-1977-a-2014/records?limit=20');
+    const limit = 20
+    const response = await fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/les-conseillers-de-paris-de-1977-a-2014/records?limit=${limit}&offset=${offset}`);
     const apiReturn = await response.json();
-    console.log(apiReturn);
-    //  return apiReturn;
 
-     const div = document.getElementById("app")
+    for (let i = 0; i < apiReturn.results.length; i++) {
+      const data = apiReturn.results[i];
 
-     for (let i =0; i < apiReturn.results.length; i++){
+      // Création de div pour chaque conseillet qui sera stylisée comme une card
+      const card = document.createElement("div")
+      card.className = "card-person"
 
-const list = document.createElement("li")
-list.innerHTML = `  <img src="${apiReturn.results[i].image.url}">
-                  <h2> Prénom : ${apiReturn.results[i].prenom} ${apiReturn.results[i].nom}</h2> 
-                 <h3> Mandature :${apiReturn.results[i].mandature}</h3>
-                  <h3> Secteur : ${apiReturn.results[i].secteur}</h3>
-                  <h3> Élue le : ${apiReturn.results[i].elu_e_le}</h3>`
+      // Infos principales : (sécurité pour éviter les erreurs si l’image n’existe pas.)
+      const list = document.createElement("li")
+      list.innerHTML = `
+       <img class="photo" src="${data.image?.url || ''}"/> 
+        <h2 class="nom">${data.prenom} ${data.nom}</h2> 
+        <h3> Mandature : ${data.mandature}</h3>
+        <h3> Secteur : ${data.secteur}</h3>
+        <h3> Élue le : ${data.elu_e_le}</h3>
+      `
+      card.appendChild(list)
 
-div.appendChild(list)
 
-     } 
-     
+
+      // Infos supplémentaires cachées
+      const listButton = document.createElement("li")
+      listButton.innerHTML = `
+        <h3> Né le : ${data.ne_e_le} à ${data.ville_de_naissance}</h3>
+        <h3> Groupe politique : ${data.groupe_politique}</h3>
+        <h3> Fonction : ${data.groupe_politique_fonctions}</h3>
+        <h3> Profession : ${data.profession}</h3>
+      `
+      listButton.style.display = "none"
+      card.appendChild(listButton)
+
+
+      // Bouton "Voir plus / Voir moins"
+      const button = document.createElement("button")
+      button.textContent = "Voir plus"
+      card.appendChild(button)
+
+      button.addEventListener("click", () => {
+        if (listButton.style.display === "none") {
+          listButton.style.display = "block"
+          button.textContent = "Voir moins"
+        } else {
+          listButton.style.display = "none"
+          button.textContent = "Voir plus"
+        }
+      })
+
+      // Ajouter la card au conteneur, 
+      cardsContainer.appendChild(card)
+
+      // Stocker la card pour la recherche, Affiche la card sur la page.
+      personCards.push(card)
     }
-    
-    catch(error){
-    console.log(error)
 
+    // Recherche en direct : Dès que l’utilisateur tape dans la barre, le code filtre les cards par nom.
+
+    searchBar.addEventListener("input", (e) => {
+      const value = e.target.value.toLowerCase()
+      personCards.forEach(card => {
+        const name = card.querySelector(".nom").textContent.toLowerCase()
+        if (name.includes(value)) {
+          card.style.display = "block"
+        } else {
+          card.style.display = "none"
+        }
+      })
+    });
+
+  } catch (error) {
+    console.log(error)
   }
 }
 
+// Clic sur "Charger plus"
+loadBtnMore.addEventListener("click", async () => {
+  offset += 20      // Pour récuperer les 20
+  await fetchApi() // Appelle fetch api pour ajouter les noiuvelles cards
+})
 
+// Premier chargement
 fetchApi()
- 
